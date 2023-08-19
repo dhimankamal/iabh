488ff4754fbc6b178a143b4a99dc048c54f76b78
kamal <dhimank079@gmail.com>
2023-08-19 00:50:53 -0700

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+       <h1 className="font-bold text-2xl">Coming Soon</h1>
+    </main>
+  )
+}
