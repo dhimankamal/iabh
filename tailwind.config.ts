d6fea4132ab977b8e18a22c26b9b9bb9995c8527
kamal <dhimank079@gmail.com>
2023-09-09 19:01:29 +0530

    hero section

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,12 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors:{
+        grey:{
+          "10":"#EBEBEB",
+          "20": "#D9D9D9"
+        }
+      }
     },
   },
   plugins: [],
