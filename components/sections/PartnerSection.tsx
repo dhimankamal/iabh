e1536e369174cdb7e0107f5d8c3bd15b9ec58ce7
kamal <dhimank079@gmail.com>
2023-11-05 18:17:49 +0530

    fix build

--- components/sections/PartnerSection.tsx
+++ components/sections/PartnerSection.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function PartnerSection () {
   return (
     <section className='text-gray-600 body-font'>
-      <div className='container px-5 py-24 mx-auto'>
+      {/* <div className='container px-5 py-24 mx-auto'>
         <div className='flex flex-col text-center w-full mb-20'>
           <h1 className='sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900'>
             Master Cleanse Reliac Heirloom
@@ -97,7 +97,7 @@ export default function PartnerSection () {
             </div>
           </div>
         </div>
-      </div>
+      </div> */}
     </section>
   )
 }
